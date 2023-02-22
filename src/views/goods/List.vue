e4a4104176de413453f51c2f32469018840d6701
zhouwang <2062057284@qq.com>
2023-02-22 23:48:18 +0800

    完成商品单规格设置开发

--- src/views/goods/List.vue
+++ src/views/goods/List.vue
@@ -1,106 +1,50 @@
 <template>
   <div id="goods" :style="{ height: `${$windowHeight - 60 - 44 - 24}px` }">
     <div class="goods-aside">
-      <el-tabs
-        tab-position="left"
-        v-model="queryform.tab"
-        class="h-[100%]"
-        @tab-change="getData(pager, { tab: queryform.tab })"
-      >
-        <el-tab-pane
-          v-for="(item, index) in tabbars"
-          :key="index"
-          :label="item.name"
-          :name="item.key"
-        ></el-tab-pane>
+      <el-tabs tab-position="left" v-model="queryform.tab" class="h-[100%]"
+        @tab-change="getData(pager, { tab: queryform.tab })">
+        <el-tab-pane v-for="(item, index) in tabbars" :key="index" :label="item.name" :name="item.key"></el-tab-pane>
       </el-tabs>
     </div>
     <div class="goods-main">
       <!-- 搜索 -->
-      <Search
-        v-model="queryform"
-        :rules="queryRules"
-        @search="handleQuery"
-        @reset="getData(pager)"
-        @resetCreateForm="queryform.category_id = null"
-      >
+      <Search v-model="queryform" :rules="queryRules" @search="handleQuery" @reset="getData(pager)"
+        @resetCreateForm="queryform.category_id = null">
         <SearchItem :prop="'title'">
-          <el-input
-            v-model="queryform.title"
-            size="small"
-            placeholder="商品名称"
-          ></el-input>
+          <el-input v-model="queryform.title" size="small" placeholder="商品名称"></el-input>
         </SearchItem>
         <template #moreSearch>
           <SearchItem :prop="'category_id'">
-            <el-select
-              v-model="queryform.category_id"
-              placeholder="商品分类"
-              size="small"
-              clearable
-            >
-              <el-option
-                v-for="item in category_list"
-                :key="item.id"
-                :label="item.name"
-                :value="item.id"
-              >
+            <el-select v-model="queryform.category_id" placeholder="商品分类" size="small" clearable>
+              <el-option v-for="item in category_list" :key="item.id" :label="item.name" :value="item.id">
               </el-option>
             </el-select>
           </SearchItem>
         </template>
       </Search>
-      <el-card
-        shadow="never"
-        class="border-0 relative"
-        :style="{ height: `${$windowHeight - 60 - 44 - 22 - 78}px` }"
-      >
+      <el-card shadow="never" class="border-0 relative" :style="{ height: `${$windowHeight - 60 - 44 - 22 - 78}px` }">
         <!-- 新增和刷新 -->
-        <ListHeader
-          layout="create,delete,refresh"
-          @create="handleCreate"
-          @refresh="getData(pager)"
-          @delete="handleDelete(multiSelectionIds)"
-        >
-          <el-button
-            v-for="item in upAndDown"
-            size="small"
-            :icon="item ? 'Top' : 'Bottom'"
-            type="info"
-            plain
-            @click="handleStatusChange(item, multiSelectionIds)"
-          >
+        <ListHeader layout="create,delete,refresh" @create="handleCreate" @refresh="getData(pager)"
+          @delete="handleDelete(multiSelectionIds)">
+          <el-button v-for="item in upAndDown" size="small" :icon="item ? 'Top' : 'Bottom'" type="info" plain
+            @click="handleStatusChange(item, multiSelectionIds)">
             {{ item ? "上架" : "下架" }}
           </el-button>
         </ListHeader>
 
-        <el-table
-          :data="tableData"
-          stripe
-          style="width: 100%"
-          v-loading="loading"
-          ref="multipleTableRef"
-          @selection-change="handleSelectionChange"
-          :max-height="$windowHeight - (60 + 44 + 22 + 78 + 48 + 80)"
-        >
+        <el-table :data="tableData" stripe style="width: 100%" v-loading="loading" ref="multipleTableRef"
+          @selection-change="handleSelectionChange" :max-height="$windowHeight - (60 + 44 + 22 + 78 + 48 + 80)">
           <el-table-column type="selection" width="55" />
           <el-table-column label="商品" width="300">
             <template #default="{ row }">
               <div class="flex items-center">
-                <el-image
-                  :src="row.cover"
-                  fit="cover"
-                  :lazy="true"
-                  class="w-[50px] h-[50px] mr-3 rounded"
-                ></el-image>
+                <el-image :src="row.cover" fit="cover" :lazy="true" class="w-[50px] h-[50px] mr-3 rounded"></el-image>
                 <div class="flex-1">
                   <p>{{ row.title }}</p>
                   <div>
                     <span class="text-rose-500">￥{{ row.min_price }}</span>
                     <el-divider direction="vertical" />
-                    <span class="text-gray-500 text-xs"
-                      >￥{{ row.min_oprice }}</span
-                    >
+                    <span class="text-gray-500 text-xs">￥{{ row.min_oprice }}</span>
                   </div>
                   <p class="text-gray-400 text-xs mb-1">
                     分类：{{ (row.category && row.category.name) || "未分类" }}
@@ -112,15 +56,8 @@
               </div>
             </template>
           </el-table-column>
-          <el-table-column
-            v-for="(item, index) in columns"
-            :key="index"
-            :prop="item.prop"
-            :label="item.label"
-            :width="item.with || 'auto'"
-            :align="item.align || ''"
-            :formatter="item.formatter"
-          />
+          <el-table-column v-for="(item, index) in columns" :key="index" :prop="item.prop" :label="item.label"
+            :width="item.with || 'auto'" :align="item.align || ''" :formatter="item.formatter" />
           <el-table-column label="状态" width="100" align="center">
             <template #default="{ row }">
               <el-tag :type="row.status ? 'success' : 'danger'">
@@ -128,32 +65,18 @@
               </el-tag>
             </template>
           </el-table-column>
-          <el-table-column
-            label="审核状态"
-            width="120"
-            align="center"
-            v-if="queryform.tab !== 'delete'"
-          >
+          <el-table-column label="审核状态" width="120" align="center" v-if="queryform.tab !== 'delete'">
             <template #default="{ row }">
               <div v-if="row.ischeck == 0">
                 <el-button type="success" size="small" plain>
                   审核通过
                 </el-button>
-                <el-button
-                  type="danger"
-                  size="small"
-                  plain
-                  class="mt-2"
-                  style="margin-left: 0"
-                >
+                <el-button type="danger" size="small" plain class="mt-2" style="margin-left: 0">
                   审核拒绝
                 </el-button>
               </div>
               <span v-else>
-                <el-tag
-                  :type="row.ischeck == 1 ? 'success' : 'danger'"
-                  effect="dark"
-                >
+                <el-tag :type="row.ischeck == 1 ? 'success' : 'danger'" effect="dark">
                   {{ row.ischeck == 1 ? "通过" : "拒绝" }}
                 </el-tag>
               </span>
@@ -162,47 +85,18 @@
           <el-table-column label="操作" align="center">
             <template #default="{ row }">
               <div class="opr" v-if="queryform.tab !== 'delete'">
-                <el-button
-                  size="small"
-                  type="primary"
-                  text
-                  icon="EditPen"
-                  @click="handleEdit(row)"
-                  >修改</el-button
-                >
-                <el-button size="small" type="primary" text icon="List"
-                  >商品规格</el-button
-                >
-                <el-button
-                  size="small"
-                  :type="row.goods_banner.length ? 'primary' : 'danger'"
-                  text
-                  icon="Picture"
-                  :loading="row.bannerStatus"
-                  @click="setGoodsBanners(row)"
-                  >设置轮播图</el-button
-                >
-                <el-button
-                  size="small"
-                  :type="row.content ? 'primary' : 'danger'"
-                  text
-                  icon="Tickets"
-                  :loading="row.contentStatus"
-                  @click="setGoodsContent(row)"
-                  >商品详情</el-button
-                >
+                <el-button size="small" type="primary" text icon="EditPen" @click="handleEdit(row)">修改</el-button>
+                <el-button size="small" type="primary" text icon="List" :loading="row.skusStatus"
+                  @click="setGoodsSkus(row)">商品规格</el-button>
+                <el-button size="small" :type="row.goods_banner.length ? 'primary' : 'danger'" text icon="Picture"
+                  :loading="row.bannerStatus" @click="setGoodsBanners(row)">设置轮播图</el-button>
+                <el-button size="small" :type="row.content ? 'primary' : 'danger'" text icon="Tickets"
+                  :loading="row.contentStatus" @click="setGoodsContent(row)">商品详情</el-button>
 
-                <el-popconfirm
-                  title="是否删除该商品?"
-                  confirm-button-text="确认"
-                  cancel-button-text="取消"
-                  width="158px"
-                  @confirm="handleDelete(scope.row.id)"
-                >
+                <el-popconfirm title="是否删除该商品?" confirm-button-text="确认" cancel-button-text="取消" width="158px"
+                  @confirm="handleDelete(scope.row.id)">
                   <template #reference>
-                    <el-button size="small" type="primary" icon="Delete" text
-                      >删除</el-button
-                    >
+                    <el-button size="small" type="primary" icon="Delete" text>删除</el-button>
                   </template>
                 </el-popconfirm>
               </div>
@@ -211,70 +105,34 @@
           </el-table-column>
         </el-table>
         <div class="pagination">
-          <el-pagination
-            @current-change="changeCurrent"
-            background
-            layout="prev, pager, next"
-            :total="pager.total"
-            :page-size="pager.limit"
-            :current-page="pager.page"
-          />
+          <el-pagination @current-change="changeCurrent" background layout="prev, pager, next" :total="pager.total"
+            :page-size="pager.limit" :current-page="pager.page" />
         </div>
       </el-card>
       <Banners @reloadData="getData(pager)" ref="bannersForm"></Banners>
       <Content @reloadData="getData(pager)" ref="contentForm"></Content>
+      <Skus @reloadData="getData(pager)" ref="skusForm"></Skus>
     </div>
-    <FormDrawer
-      :title="isEdit ? '编辑' : '新增'"
-      ref="formDrawerRef"
-      @submit="handleSubmit"
-      @reset="reset"
-    >
-      <el-form
-        :model="createForm"
-        ref="DrawerRef"
-        :rules="rules"
-        label-width="100px"
-        :inline="false"
-      >
+    <FormDrawer :title="isEdit ? '编辑' : '新增'" ref="formDrawerRef" @submit="handleSubmit" @reset="reset">
+      <el-form :model="createForm" ref="DrawerRef" :rules="rules" label-width="100px" :inline="false">
         <el-form-item label="商品名称" prop="title">
-          <el-input
-            v-model="createForm.title"
-            placeholder="请输入商品名称,不能超过60个字符"
-          ></el-input>
+          <el-input v-model="createForm.title" placeholder="请输入商品名称,不能超过60个字符"></el-input>
         </el-form-item>
         <el-form-item label="封面" prop="cover">
           <ChooseImage v-model:avatar="createForm.cover"></ChooseImage>
         </el-form-item>
         <el-form-item label="商品分类" prop="category_id">
-          <el-select
-            v-model="createForm.category_id"
-            placeholder="请选择商品分类"
-          >
-            <el-option
-              v-for="item in category_list"
-              :key="item.id"
-              :label="item.name"
-              :value="item.id"
-            />
+          <el-select v-model="createForm.category_id" placeholder="请选择商品分类">
+            <el-option v-for="item in category_list" :key="item.id" :label="item.name" :value="item.id" />
           </el-select>
         </el-form-item>
         <el-form-item label="商品描述" prop="desc">
-          <el-input
-            v-model="createForm.desc"
-            placeholder="选填，请输入商品描述"
-            type="textarea"
-          ></el-input>
+          <el-input v-model="createForm.desc" placeholder="选填，请输入商品描述" type="textarea"></el-input>
         </el-form-item>
         <el-form-item label="单位" prop="unit">
-          <el-cascader
-            v-model="createForm.unit"
-            :options="unitList"
-            :show-all-levels="false"
-            :props="{
-              emitPath: false,
-            }"
-          />
+          <el-cascader v-model="createForm.unit" :options="unitList" :show-all-levels="false" :props="{
+            emitPath: false,
+          }" />
         </el-form-item>
         <el-form-item label="总库存" prop="stock">
           <el-input v-model="createForm.stock" type="number" style="width: 40%">
@@ -282,29 +140,17 @@
           </el-input>
         </el-form-item>
         <el-form-item label="库存预警" prop="min_stock">
-          <el-input
-            v-model="createForm.min_stock"
-            type="number"
-            style="width: 40%"
-          >
+          <el-input v-model="createForm.min_stock" type="number" style="width: 40%">
             <template #append>{{ createForm.unit }}</template>
           </el-input>
         </el-form-item>
         <el-form-item label="最低销售价" prop="min_price">
-          <el-input
-            v-model="createForm.min_price"
-            type="number"
-            style="width: 40%"
-          >
+          <el-input v-model="createForm.min_price" type="number" style="width: 40%">
             <template #append>元</template>
           </el-input>
         </el-form-item>
         <el-form-item label="最低原价" prop="min_oprice">
-          <el-input
-            v-model="createForm.min_oprice"
-            type="number"
-            style="width: 40%"
-          >
+          <el-input v-model="createForm.min_oprice" type="number" style="width: 40%">
             <template #append>元</template>
           </el-input>
         </el-form-item>
@@ -337,9 +183,11 @@ import { tabbars, unitList } from "@/views/goods/parameter";
 import { useTableInit, useInitForm } from "@/composables/useCommon";
 import Banners from "@/views/goods/Banners.vue";
 import Content from "@/views/goods/Content.vue";
+import Skus from "@/views/goods/Skus.vue";
 const selectData = ref([]);
 const bannersForm = ref(null);
 const contentForm = ref(null);
+const skusForm = ref(null);
 const upAndDown = computed(() => {
   let data = [];
   switch (queryform.tab) {
@@ -401,6 +249,7 @@ const {
     tableData.value = res.list.map((item) => {
       item.bannerStatus = false;
       item.contentStatus = false;
+      item.skusStatus = false
       return item;
     });
   },
@@ -440,6 +289,7 @@ const {
 
 const setGoodsBanners = (row) => bannersForm.value.open(row);
 const setGoodsContent = (row) => contentForm.value.open(row);
+const setGoodsSkus = (row) => skusForm.value.open(row);
 </script>
 
 <style scoped lang="scss">
---
+++ src/views/goods/Skus.vue
@@ -0,0 +1,141 @@
+<template>
+  <FormDrawer title="设置商品规格" ref="formDrawerRef" destroyOnClose @submit="handleSubmit"
+    :size="skusForm.sku_type ? '70%' : '40%'">
+    <el-form :model="skusForm">
+      <el-form-item label="规格类型">
+        <el-radio-group v-model="skusForm.sku_type">
+          <el-radio :label="0" border>单规格</el-radio>
+          <el-radio :label="1" border>多规格</el-radio>
+        </el-radio-group>
+      </el-form-item>
+      <template v-if="!skusForm.sku_type">
+        <el-form-item label="市场价格">
+          <el-input v-model="skusForm.sku_value.oprice" placeholder="请输入市场价格" style="width: 50%">
+            <template #append>元</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="销售价格">
+          <el-input v-model="skusForm.sku_value.pprice" placeholder="请输入销售价格" style="width: 50%">
+            <template #append>元</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="成本价格">
+          <el-input v-model="skusForm.sku_value.cprice" placeholder="请输入成本价格" style="width: 50%">
+            <template #append>元</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="商品重量">
+          <el-input v-model="skusForm.sku_value.weight" placeholder="请输入商品重量" style="width: 50%">
+            <template #append>公斤</template>
+          </el-input>
+        </el-form-item>
+        <el-form-item label="商品体积">
+          <el-input v-model="skusForm.sku_value.volume" placeholder="请输入商品体积" style="width: 50%">
+            <template #append>立方米</template>
+          </el-input>
+        </el-form-item>
+      </template>
+      <template v-else>
+        多规格
+      </template>
+    </el-form>
+  </FormDrawer>
+</template>
+
+<script setup>
+import { ref, reactive, nextTick } from "vue";
+import FormDrawer from "@/components/FormDrawer.vue";
+import { notification } from "@/utils/utils";
+import { cloneDeep } from "lodash";
+import goodsApi from "@/api/goods";
+const formDrawerRef = ref(null);
+const initSkus = {
+  sku_value: {
+    oprice: 0,
+    pprice: 0,
+    cprice: 0,
+    weight: 0,
+    volume: 0
+  }, //单规格属性（sku_type=0时必填）
+  goodsSkus: [
+    {
+      skus: [
+        {
+          goods_skus_card_id: "",
+          name: "",
+          order: 50,
+          value: "",
+          id: "",
+          text: ""
+        }
+      ],
+      image: "",
+      pprice: 0,
+      oprice: 0,
+      cprice: 0,
+      stock: 0,
+      volume: 0,
+      weight: 0,
+      code: "",
+      goods_id: null
+    }
+  ] //多规格属性（sku_type=1时必填）
+}
+const skusForm = reactive({
+  sku_type: 0,//0单规格，1多规格
+  sku_value: cloneDeep(initSkus.sku_value),
+  goodsSkus: cloneDeep(initSkus.goodsSkus)
+});
+const goodsId = ref(0);
+
+const open = async (row) => {
+  row.skusStatus = true;
+  try {
+    goodsId.value = row.id;
+    const res = await goodsApi.readGoods(goodsId.value);
+  console.log(res)
+    skusForm.sku_type = res.sku_type;
+    skusForm.sku_value = res.sku_value || cloneDeep(initSkus.sku_value)
+    skusForm.goodsSkus = res.goodsSkus || cloneDeep(initSkus.goodsSkus)
+    row.skusStatus = false;
+    formDrawerRef.value.open();
+  } catch (error) {
+    console.table(error);
+    notification("获取规格详情数据失败", "error");
+    row.skusStatus = false;
+  }
+};
+const handleSubmit = async () => {
+  formDrawerRef.value.showLoading();
+  try {
+    let params = {
+      sku_type: goodsId.value,
+      goodsSkus: [],
+      sku_value: {}
+    }
+    if (skusForm.sku_type) {
+      params.goodsSkus = cloneDeep(skusForm.goodsSkus)
+    } else {
+      params.sku_value = cloneDeep(skusForm.sku_value)
+    }
+    const res = await goodsApi.updateGoodsSkus(params.sku_type, params);
+    if (res) {
+      notification("商品规格修改成功");
+      formDrawerRef.value.close();
+    } else {
+      notification("商品规格修改失败", "error");
+    }
+    formDrawerRef.value.hideLoading();
+  } catch (error) {
+    notification("商品规格修改失败", "error");
+    formDrawerRef.value.close();
+    formDrawerRef.value.hideLoading();
+  }
+};
+const emits = defineEmits(["reloadData"]);
+defineExpose({
+  open,
+});
+</script>
+
+<style scoped lang="scss"></style>
