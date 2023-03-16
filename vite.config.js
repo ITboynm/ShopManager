7651feca6114dfde7b6d19469c737b9b238321c5
zhouwang <zhouwang@beyondcent.com>
2023-03-16 17:26:33 +0800

    启动真数据

--- vite.config.js
+++ vite.config.js
@@ -21,10 +21,9 @@ export default defineConfig({
     proxy: {
       "/api": {
         // 线上的接口
-        // target: "http://ceshi13.dishait.cn",
+        target: "http://ceshi13.dishait.cn",
         // mock接口
-        target:
-          "https://www.fastmock.site/mock/47dfae3baec8ed8fa9486189651d8694",
+        // target:"https://www.fastmock.site/mock/47dfae3baec8ed8fa9486189651d8694",
         changeOrigin: true,
         // 重写路由
         rewrite: (path) => path.replace(/^\/api/, ""),
