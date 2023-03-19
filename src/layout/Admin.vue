44d1efac2296328ce3f7e7a7b0ccdb24116eee9a
zhouwang <2062057284@qq.com>
2023-03-19 17:06:59 +0800

    更改交互速度

--- src/layout/Admin.vue
+++ src/layout/Admin.vue
@@ -57,10 +57,10 @@ import FTagList from './components/FTagList.vue';
 
 .fade-enter-active,
 .fade-leave-active {
-    transition: all 0.3s;
+    transition: all 0.2s;
 }
 
 .fade-enter-active {
-    transition-delay: 0.3s;
+    transition-delay: 0.2s;
 }
 </style>
